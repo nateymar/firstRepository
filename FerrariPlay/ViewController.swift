718f1a0e1a419bef43f01125c6b31afb68604942
Nigel Wong <nigelwong@nigels-macbook-pro.local>
2016-08-02 17:58:41 +0800

    Initial Commit

---
+++ FerrariPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FerrariPlay
+//
+//  Created by Nigel Wong on 2/08/2016.
+//  Copyright © 2016 Nigel Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
